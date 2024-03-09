14bc854659c7c5451ebcb9ffa6845e68cf2de0fc
szhanggg <stevez9101@gmail.com>
2024-03-08 21:13:13 -0500

    updated for 2024

--- leaderboard.py
+++ leaderboard.py
@@ -3,26 +3,29 @@ import streamlit as st
 import altair as alt
 from streamlit_autorefresh import st_autorefresh
 
-division = st.selectbox('Please select a division.', ['Bernoulli', 'Germain', ''])
+division = st.selectbox('Please select a division.', ['Godel', 'Brahmagupta'])
 
 count = st_autorefresh(interval=2000, limit=1000000, key="refresh")
 
 def load_data(division):
-    if division == 'Bernoulli':
-        return pd.read_csv("https://docs.google.com/spreadsheets/d/1dZlXlSqBmgKh771fyiMSRir70wNc6_ccpfMjEwm7tok/export?format=csv&gid=946925391", skiprows=1)
-    elif division == 'Germain':
-        return pd.read_csv("https://docs.google.com/spreadsheets/d/1YDCOC-5evKzticNx6C_SZcDCGERPjnQzfkgGPwz-evg/export?format=csv&gid=946925391", skiprows=1)
-    else:
-        return pd.read_csv("https://docs.google.com/spreadsheets/d/12ZsQRH-s-nFwu-evY3gquxxO2Zym7gCjLZDbko7Kvns/export?format=csv&gid=946925391", skiprows=1)
+    sheet = pd.read_csv("https://docs.google.com/spreadsheets/d/1m_S9K3eirLCN-LJ1IHOp7u04ESZ1Vlaq384FIsdv__Y/export?format=csv", skiprows=3)
+    if division == 'Godel':
+        # Take only first 10 rows from 0 to 9
+        sheet = sheet.iloc[0:10]
+    elif division == 'Brahmagupta':
+        sheet = sheet.iloc[15:25]
+    # Take only team name and score columns
+    sheet = sheet[['Team Name', 'Score']]
+    return sheet
 
 leaderboard_data = load_data(division)
-leaderboard_data = leaderboard_data.sort_values('Total', ascending=False).reset_index(drop=True)
+leaderboard_data = leaderboard_data.sort_values('Score', ascending=False).reset_index(drop=True)
 print(leaderboard_data.head())
 leaderboard_data['Team Name'] = (leaderboard_data.index + 1).astype(str) + '. ' + leaderboard_data['Team Name'].astype(str)
 
 # Create the base chart
 base = alt.Chart(leaderboard_data).encode(
-    alt.X('Total:Q', title='Points'),
+    alt.X('Score:Q', title='Score'),
     alt.Y('Team Name:N', title='Team Name', sort=alt.EncodingSortField(field='Total', op='max', order='descending'))
 )
 
